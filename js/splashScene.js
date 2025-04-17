6d9f7c0c04413b711380d22c881b895972615936
bain-liao-1 <liao.bain@stu.ocsb.ca>
2025-04-17 16:48:55 +0000

    fixed js

--- js/splashScene.js
+++ js/splashScene.js
@@ -11,9 +11,9 @@ class SplashScene extends Phaser.Scene {
     super({ key: "splashScene" })
   }
 
-/**
- * @param {object} data 
- */
+  /**
+   * @param {object} data 
+   */
   inti(data) {
     this.cameras.main.setBackgroundColor("ffffff")
   }
@@ -22,17 +22,17 @@ class SplashScene extends Phaser.Scene {
     console.log("Splash Scene")
   }
 
-/**
- * @param {object} data 
- */
+  /**
+   * @param {object} data 
+   */
   create(data) {
     // pass
   }
 
-/**
- * @param {number} time
- * @param {number} delta 
- */
+  /**
+   * @param {number} time
+   * @param {number} delta 
+   */
   update(time, delta) {
     // pass
   }
