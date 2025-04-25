339ed5821a771081c8c74fbc47832e3093d8326c
bain-liao-1 <liao.bain@stu.ocsb.ca>
2025-04-25 16:36:50 +0000

    updated splashScene.js to show image

--- js/splashScene.js
+++ js/splashScene.js
@@ -13,6 +13,8 @@ class SplashScene extends Phaser.Scene {
    */
   constructor () {
     super({ key: 'splashScene' })
+
+    this.splashSceneBackgroundImage = null
   }
 
   /**
@@ -31,6 +33,7 @@ class SplashScene extends Phaser.Scene {
    */
   preload () {
     console.log('Splash Scene')
+    this.load.image('splashSceneBackground', './assets/splashSceneImage.png')
   }
 
   /**
@@ -39,7 +42,9 @@ class SplashScene extends Phaser.Scene {
    * @param {object} data Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   create (data) {
-    // pass
+    this.splashSceneBackgroundImage = this.add.sprite(0, 0, 'splashSceneBackground')
+    this.splashSceneBackgroundImage.x = 1920 / 2
+    this.splashSceneBackgroundImage.y = 1080 / 2
   }
 
   /**
@@ -49,7 +54,9 @@ class SplashScene extends Phaser.Scene {
    * @param {number} delta The delta time in ms since the last frame.
    */
   update (time, delta) {
-    this.scene.switch('titleScene')
+    if (time > 3000) {
+      this.scene.switch('titleScene')
+    }
   }
 }
 
