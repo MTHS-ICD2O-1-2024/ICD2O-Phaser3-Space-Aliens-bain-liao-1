0673582128cbe91d56134340fda09e32704c7b7c
bain-liao-1 <liao.bain@stu.ocsb.ca>
2025-05-26 16:56:31 +0000

    added a score reset when you press play again

--- js/gameScene.js
+++ js/gameScene.js
@@ -86,6 +86,7 @@ class GameScene extends Phaser.Scene {
       this.gameOverText = this.add.text(1920 / 2, 1080 / 2, 'Game Over!\nClick to play again.', this.gameOverTextStyle).setOrigin(0.5)
       this.gameOverText.setInteractive({ useHandCursor: true })
       this.gameOverText.on('pointerdown', () => this.scene.start('gameScene'))
+      this.score = 0
     }.bind(this))
   }
 
